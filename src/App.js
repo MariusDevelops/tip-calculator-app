0437f1b8b4cf9e91dc123b45b2722f85ef9f376a
MariusDevelops <brazasmarius@gmail.com>
2021-12-21 22:49:19 +0100

    Basic HTML and SCSS setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./dist/App.css";
+import logo from "./images/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header className="logo">
+        <img src={logo} alt="logo" />
       </header>
-    </div>
+      <div className="wrapper">
+        <section>
+          <p>Bill</p>
+          <input type="text" />
+          <div>
+            <p>Select Tip</p>
+            <div>5%</div>
+            <div>10%</div>
+            <div>15%</div>
+            <div>25%</div>
+            <div>50%</div>
+            <input type="text" />
+          </div>
+          <div>
+            <p>Number of People</p>
+            <input type="text" />
+          </div>
+        </section>
+        <section>
+          <div>
+            <p>
+              Tip Amount <br />/ person
+            </p>
+            <div className="tipOutput"></div>
+          </div>
+          <div>
+            <p>
+              Total <br />/ person
+            </p>
+            <div className="totalOutput"></div>
+          </div>
+        </section>
+      </div>
+    </main>
   );
 }
 
