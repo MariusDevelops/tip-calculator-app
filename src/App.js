db72c719a4f4995bb660486ac64447c83e9181ab
MariusDevelops <brazasmarius@gmail.com>
2021-12-22 18:22:44 +0100

    Mobile 375px HTML and SCSS design

--- src/App.js
+++ src/App.js
@@ -8,36 +8,53 @@ function App() {
         <img src={logo} alt="logo" />
       </header>
       <div className="wrapper">
-        <section>
-          <p>Bill</p>
-          <input type="text" />
-          <div>
-            <p>Select Tip</p>
-            <div>5%</div>
-            <div>10%</div>
-            <div>15%</div>
-            <div>25%</div>
-            <div>50%</div>
-            <input type="text" />
+        <section className="section1">
+          <label htmlFor="">Bill</label>
+          <input type="number" className="input bill-input" id="bill-input" />
+          <label htmlFor="">Select Tip %</label>
+          <div className="tip">
+            <div className="tips tip-5">5%</div>
+            <div className="tips tip-10">10%</div>
+            <div className="tips tip-15 active-tip">15%</div>
+            <div className="tips tip-25">25%</div>
+            <div className="tips tip-50">50%</div>
+            <div id="tip-custom">
+              <input
+                type="number"
+                className="tip-custom"
+                placeholder="Custom"
+              />
+            </div>
           </div>
-          <div>
-            <p>Number of People</p>
-            <input type="text" />
+          <div className="peopleLabel">
+            <label htmlFor="">Number of People</label>
+            <label htmlFor="" className="error">
+              Can't be zero
+            </label>
           </div>
+          <input type="number" className="input people-input" />
         </section>
-        <section>
-          <div>
-            <p>
-              Tip Amount <br />/ person
-            </p>
-            <div className="tipOutput"></div>
+
+        <section className="result">
+          <div className="tip-amount">
+            <div className="text">
+              <p>Tip Amount</p>
+              <p className="person">/ person</p>
+            </div>
+            <div className="amount" id="tip-amount">
+              $4.27
+            </div>
           </div>
-          <div>
-            <p>
-              Total <br />/ person
-            </p>
-            <div className="totalOutput"></div>
+          <div className="total">
+            <div className="text">
+              <p>Total</p>
+              <p className="person">/ person</p>
+            </div>
+            <div className="amount" id="total-amount">
+              $32.79
+            </div>
           </div>
+          <div className="reset">RESET</div>
         </section>
       </div>
     </main>
